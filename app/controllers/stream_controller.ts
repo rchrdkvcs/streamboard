a793993d02a14b68250555fa8ffa3b26ec358fbc
gumimacihun <gumimacihun@gmail.com>
2024-05-02 18:45:56 +0200

    core(refactor): remove unused code

--- app/controllers/stream_controller.ts
+++
@@ -1,7 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-
-export default class StreamController {
-  async index({ inertia }: HttpContext) {
-    return inertia.render('stream')
-  }
-}
