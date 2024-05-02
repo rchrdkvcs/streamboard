3c1c52e0d1b361f278ec4650aee8dd381baf717a
gumimacihun <gumimacihun@gmail.com>
2024-05-02 19:28:14 +0200

    core: add support for real-time data transmission

--- inertia/pages/stream.tsx
+++ inertia/pages/stream.tsx
@@ -1,4 +1,24 @@
+import { Transmit } from '@adonisjs/transmit-client'
+import { useEffect } from 'react'
+
 export default function Stream() {
+  useEffect(() => {
+    const transmit = new Transmit({
+      baseUrl: 'http://localhost:3333',
+    })
+
+    const subscription = transmit.subscription('tasks')
+    void subscription.create()
+
+    subscription.onMessage((task) => {
+      console.log(task)
+    })
+
+    return () => {
+      transmit.close()
+    }
+  }, [])
+
   return (
     <main className="flex items-start justify-start w-full h-screen">
       <h1>Stream</h1>
--- start/routes.ts
+++ start/routes.ts
@@ -7,30 +7,20 @@
 |
 */
 
-// const DashboardController = () => import('#controllers/dashboard_controller')
-// const StreamController = () => import('#controllers/stream_controller')
-// const StoreEventController = () => import('#controllers/dashboard/store_event_controller')
-// const StoreQstController = () => import('#controllers/dashboard/store_question_controller')
+import router from '@adonisjs/core/services/router'
+
 const IndexEventsController = () => import('#events/controllers/index_events_controller')
 const ShowEventController = () => import('#events/controllers/show_event_controller')
 const StoreEventController = () => import('#events/controllers/store_event_controller')
 const StoreTaskController = () => import('#tasks/controllers/store_tasks_controller')
-import router from '@adonisjs/core/services/router'
-
-// const HomeController = () => import('#controllers/home_controller')
-
-// router.get('/', [HomeController, 'show'])
-
-// router.get('/stream-assets', [StreamController, 'index'])
-
-// router.get('/dashboard', [DashboardController, 'index'])
-// router.get('/dashboard/:id/question', [DashboardController, 'question'])
-// router.get('/dashboard/:id/game', [DashboardController, 'game'])
-
-// router.post('/store-event', [StoreEventController, 'execute'])
-// router.post('/store-qst', [StoreQstController, 'execute'])
 
 router.get('/events', [IndexEventsController])
 router.get('/event/:id', [ShowEventController])
+
 router.post('/events/store', [StoreEventController])
+
 router.post('/task/store', [StoreTaskController])
+
+router.get('/stream', ({ inertia }) => {
+  return inertia.render('stream')
+})
